95c36eaf7f37eb825946e2ec2823d5e60119c531
vagrant <vagrant@homestead>
2018-06-22 09:23:01 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://secure-shore-13994.herokuapp.com/" target="_blank">Ziv</a> 编写 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="">联系我们</a></p>
+    </div>
+</footer>
