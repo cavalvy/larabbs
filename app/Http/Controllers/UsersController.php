6953757fe98f06d77a77e7370c842b3d3afddc0e
vagrant <vagrant@homestead>
2018-06-25 08:07:35 +0000

    简单用户界面编辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 
 class UsersController extends Controller
@@ -17,13 +17,14 @@ class UsersController extends Controller
          return view('users.show',compact('user'));
     }
 
-    public function update(User $user)
+    public function update(UserRequest $require,User $user)
     {
-
+        $user->update($require->all());
+        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
     }
 
     public function edit(User $user)
     {
-
+        return view('users.edit',compact('user'));
     }
 }
