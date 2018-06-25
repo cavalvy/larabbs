d7fe4e16b4113b474236a7ee853684e65502021a
vagrant <vagrant@homestead>
2018-06-25 09:29:21 +0000

    Add PostgreSQL

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -7,3 +7,30 @@ function route_class()
 {
     return str_replace('.','-',Route::currentRouteName());
 }
+
+/**
+ * 不同的运行环境来指定数据库配置信息
+ * @return array
+ */
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
