799a1a20f30d2445cfc6c8c3ba71394045820c9a
vagrant <vagrant@homestead>
2018-07-01 08:50:36 +0000

    话题 内容页

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title','laraBBS')</title>
+    <meta name="description" content="@yield('description','laraBBS')">
     <!-- style -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('styles')
