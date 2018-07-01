43a0e0a465c07517b201caf97cc89155dcba6f4b
vagrant <vagrant@homestead>
2018-07-01 07:53:01 +0000

    用户能上传话题图片

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -13,11 +13,11 @@
                 <div class="well well-sm">
                     <div class="row">
                         <div class="col-md-6">
-                            <a class="btn btn-link" href="{{ route('topics.index') }}"><i class="glyphicon glyphicon-backward"></i> Back</a>
+                            <a class="btn btn-link" href="{{ route('topics.index') }}"><i class="glyphicon glyphicon-backward"></i> 返回</a>
                         </div>
                         <div class="col-md-6">
                              <a class="btn btn-sm btn-warning pull-right" href="{{ route('topics.edit', $topic->id) }}">
-                                <i class="glyphicon glyphicon-edit"></i> Edit
+                                <i class="glyphicon glyphicon-edit"></i> 编辑
                             </a>
                         </div>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,5 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 //分类列表话题
 Route::resource('categories','CategoriesController',['only' => ['show']]);
+//图片上传
+Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
